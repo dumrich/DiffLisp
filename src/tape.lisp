946114ef764cfa060ec98091e56b40e190107c81
Abhinav Chavali <abhinavchavali12@gmail.com>
2025-11-24 12:00:00 -0500

    Added value struct

--- src/tape.lisp
+++ src/tape.lisp
@@ -1,6 +1,31 @@
 ;;;; tape.lisp
 ;;;; Tape-based automatic differentiation infrastructure
 
-(in-package #:difflisp)
+(in-package #:difflisp.tape)
 
-;; Placeholder for tape-based AD implementation
+;; Implementation plan:
+;; Tape Struct
+;; - Stack of operations (struct)
+;; - Private Methods:
+;;   - (add-to-tape operation)
+;; Operation Struct
+;; - Stores a list of input Values
+;; - Stores an output Value
+;; - Stores a gradient closure. Updates each input with respect to the inputs
+;; Value Struct
+;; - Stores double-float value (initialized to zero)
+;; - Stores double-float gradient (initialized to 0.0)
+
+;; Generic value
+(defstruct (value
+            (:print-function print-variable))
+    (val 0.0)
+    (grad 0.0))
+
+(defun print-variable (v stream depth)
+  (declare (ignore depth))
+  (format stream "#<VAR val:~5,2f grad:~5,2f>"
+          (variable-val v)
+          (variable-grad v)))
+
+
