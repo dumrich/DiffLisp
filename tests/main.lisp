3653a3f2c8af7ad2113302c01545fbff1fe6356a
Abhinav Chavali <abhinavchavali12@gmail.com>
2025-11-26 12:00:00 -0500

    Added tests for grad

--- tests/main.lisp
+++ tests/main.lisp
@@ -11,3 +11,26 @@
 (in-suite difflisp-tests)
 
 ;; Placeholder for tests
+
+;; Test Value
+;; Test Operation
+;; Test Tape
+
+;; Test Gradient (default first arg)
+(defun f (x y)
+  x + y)
+
+(defun g (x y)
+  (* (f x y) 3))
+
+(test add-mul-grad
+    """Test adding and multiplying w.r.t x"""
+      (let (g-prime (grad #'g))
+          (is (g-prime 1 2) 9)))
+
+        
+;; Test Gradient (custom first arg)
+(test add-mul-grad-named
+    """Test adding and multiplying w.r.t x (named)"""
+      (let (g-prime (grad #'g :arg '(0 1)))
+          (is (g-prime 1 2) 9)))
