db46feedc118f830c80b8724f7888929a7d8197f
Abhinav Chavali <abhinavchavali12@gmail.com>
2026-01-06 17:09:04 -0500

    Added difflisp.ops package

--- src/package.lisp
+++ src/package.lisp
@@ -1,8 +1,15 @@
 ;;;; package.lisp
 
-
 (defpackage #:difflisp.tape
-  (:use #:cl))
+  (:use #:cl)
+  (:export #:number #:make-number #:number-value #:number-grad
+           #:operation #:reset-tape #:push-to-tape))
+
+(defpackage #:difflisp.ops
+  (:use #:cl #:difflisp.tape)
+  (:shadow #:* #:+)
+  (:export #:* #:+))
+  
 
 (defpackage #:difflisp
-    (:use #:cl))
+    (:use #:cl #:difflisp.tape #:difflisp.tape))
