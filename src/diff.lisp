bf5b16379551ae34ebde346e6e468c1cb527c943
Abhinav Chavali <abhinavchavali12@gmail.com>
2026-01-07 23:37:16 -0500

    Added backprop and grad function

--- src/diff.lisp
+++ src/diff.lisp
@@ -1,6 +1,31 @@
 ;;;; diff.lisp
 ;;;; Automatic differentiation core
 
-(in-package #:difflisp)
+(in-package #:difflisp.diff)
 
-;; Placeholder for automatic differentiation implementation
+(defun backward ()
+    (declare (optimize (debug 1))) ;; Low debug enables TCO
+    (if (zerop (fill-pointer *tape*))
+      nil
+      (let ((op (pop-from-tape)))
+        (funcall (operation-closure op))
+        (backward))))
+  
+  
+  
+(defun grad (f &rest)
+  (lambda (&rest args)
+    ;; Collect arguments into list of dnumbers
+    (reset-tape)
+    (let* ((nargs (mapcar #'(lambda (x)
+                              (if (dnumber-p x)
+                                  x
+                                  (make-dnumber :value x))) args))
+           (out (apply f nargs)))
+
+        ;; Set gradient
+      (if (dnumber-p out)
+        (setf (dnumber-grad out) 1.0d0)
+        (return-from grad (mapcar (constantly 0.0d0) args)))
+      (backward)
+      (mapcar #'(lambda (x) (dnumber-grad x)) nargs))))
