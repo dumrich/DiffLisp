68ca96e5415b3b071591fb6db5aa1d017cc25680
Abhinav Chavali <abhinavchavali12@gmail.com>
2026-01-07 23:36:14 -0500

    Fixed package.lisp
    Added `difflisp.diff` and fixed number->dnumber conversions.

--- src/package.lisp
+++ src/package.lisp
@@ -2,14 +2,18 @@
 
 (defpackage #:difflisp.tape
   (:use #:cl)
-  (:export #:number #:make-number #:number-value #:number-grad
-           #:operation #:reset-tape #:push-to-tape))
+  (:export #:dnumber #:make-dnumber #:dnumber-value #:dnumber-grad
+           #:operation #:reset-tape #:push-to-tape #:pop-from-tape))
 
 (defpackage #:difflisp.ops
   (:use #:cl #:difflisp.tape)
   (:shadow #:* #:+ #:- #:/)
   (:export #:* #:+ #:- #:/))
   
+(defpackage #:difflisp.diff
+  (:use #:cl #:difflisp.tape)
+  (:export #:grad #:backward))
+  
 
 (defpackage #:difflisp
-    (:use #:cl #:difflisp.tape #:difflisp.ops))
+    (:use #:cl #:difflisp.tape #:difflisp.ops #:difflisp.diff))
