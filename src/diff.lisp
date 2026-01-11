b4660749ec580b2452774638185855295091206b
Abhinav Chavali <abhinavchavali12@gmail.com>
2026-01-11 18:44:58 -0500

    Updated diff.lisp

--- src/diff.lisp
+++ src/diff.lisp
@@ -5,7 +5,7 @@
 
 (defun backward ()
     (declare (optimize (debug 1))) ;; Low debug enables TCO
-    (if (zerop (fill-pointer *tape*))
+    (if (zerop (tape-size))
       nil
       (let ((op (pop-from-tape)))
         (funcall (operation-closure op))
@@ -13,19 +13,20 @@
   
   
   
-(defun grad (f &rest)
+(defun grad (f)
   (lambda (&rest args)
     ;; Collect arguments into list of dnumbers
     (reset-tape)
     (let* ((nargs (mapcar #'(lambda (x)
                               (if (dnumber-p x)
                                   x
-                                  (make-dnumber :value x))) args))
+                                  (make-dnumber :value (coerce x 'double-float)))) args))
            (out (apply f nargs)))
 
         ;; Set gradient
       (if (dnumber-p out)
-        (setf (dnumber-grad out) 1.0d0)
-        (return-from grad (mapcar (constantly 0.0d0) args)))
-      (backward)
-      (mapcar #'(lambda (x) (dnumber-grad x)) nargs))))
+        (progn
+            (setf (dnumber-grad out) 1.0d0)
+            (backward)
+            (mapcar #'(lambda (x) (dnumber-grad x)) nargs))
+        (mapcar (constantly 0.0d0) args)))))
